62b117f9faf3bd632e1d76f6c677e59b26451d9c
zkdlyn <79004275+zkdlyn@users.noreply.github.com>
2023-12-08 13:56:44 +0800

    login_form.php

--- blogsite/login_form.php
+++ blogsite/login_form.php
@@ -58,6 +58,7 @@ if(isset($_POST['submit'])){
       <input type="password" name="password" required placeholder="enter your password">
       <input type="submit" name="submit" value="login now" class="form-btn">
       <p>don't have an account? <a href="register_form.php">register now</a></p>
+      <p><a href="user_page.php">continue without logging in</a></p>
    </form>
 
 </div>
